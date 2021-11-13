4693f25c823999f94734b47e6b5ba9e1ad49d9b5
IapetusTitan <danny.wheeler88@gmail.com>
2021-11-13 07:51:41 +0000

    Added projects page and adjusted style colours of links and headings

--- resources/js/index.js
+++ resources/js/index.js
@@ -15,8 +15,8 @@ dailyActivity[2] = 'It\'s Tuesday so I\'ll be going out on a long walk!';
 dailyActivity[3] = 'As it\'s a <em>Wednesday</em>, i\'ll probably be listening to <a href=\'https://podbay.fm/p/giant-bombcast\' target=\'blank\'>The Giant Bombcast</a>, my favourite podcast!';
 dailyActivity[4] = 'It\'s Thursday so I\'ll be going out on a long walk!';
 dailyActivity[5] = 'Happy Friday! At the moment I usually study every day, but I still get the Friday feeling because my partner Tulu will be ready for a movie night.';
-dailyActivity[6] = 'The weekend is here! That means a slow morning enjoying breakfast and coffee with my partner Tulu.  We\'ll probably listen to <a href=\'https://www.thisamericanlife.org/recommended/new-to-this-american-life\' target=\'blank\'>This American Life</a> together, check it out if you enjoy true stories and high-end audio production.';
+dailyActivity[6] = 'The weekend is here! That means a slow morning enjoying breakfast and coffee with my partner Tulu.  We\'ll probably listen to <a href=\'https://www.thisamericanlife.org/recommended/new-to-this-american-life\' target=\'blank\'>This American Life</a> together - check it out if you enjoy true stories and high-end audio production.';
 
-const dailyCode = `<p> ${dailyActivity[3]}</p>`
+const dailyCode = `<p> ${dailyActivity[today.getDay()]}</p>`
 
 document.getElementById('doing').insertAdjacentHTML('afterend', dailyCode);
